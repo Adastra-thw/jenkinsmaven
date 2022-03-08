7f8bd3c18b0cce6d6e3b12513a45be3ce7f56c66
Dani <adastra@thehackerway.com>
2022-03-08 17:55:32 +0100

    cambios

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Adastra-THW/jenkinsmaven.git', 'master') { node ->
+            node / gitConfigName('adastra')
+            node / gitConfigEmail('adastra@thehackerway.com')
         }
     }
     triggers {
